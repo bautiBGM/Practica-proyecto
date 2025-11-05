f2ca0859bf6b18bd88ca88dec2459c17a4ea86f4
BautiGayoso <Bautigayoso2001@gmail.com>
2025-11-05 17:46:03 -0300

    agregando cambios de practica

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-let nombre = "bautista"
+// import http from "node:http"
+
+// const app = http.createServer((request, response)=>{
+//     // console.log('request:', request);
+//     // console.log('response:',response);
+
+
+//     response.end("servidor de node")
+    
+// })
+
+
+// app.listen(8080,()=> 
+// console.log('server ok in localhost:8080'))
+
+import express from "express"
+
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(request,response)=>{
+    response.send("get all")
+})
+app.get("/:id",(request,response)=>{
+    response.send("get by id")
+})
+
+app.post("/",(request,response)=>{
+    console.log("request:",request);
+    
+    response.send("post")
+})
+
+app.listen(8080,()=>{
+    console.log('8080')
+})
