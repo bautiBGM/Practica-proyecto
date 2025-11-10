10f9a644c5a23b50f28f923170aa2ac271898980
BautiGayoso <Bautigayoso2001@gmail.com>
2025-11-10 16:31:24 -0300

    agregado de conexion al servidor de la base de datos y pequenios cambios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
-// import router from "./routes/router.js"
+import router from "./routes/router.js"
+import connection from "./db/connection.js"
 
 const app = express()
 
@@ -7,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-// app.use("/app",router)
+app.use("/app",router)
 
 app.use((req,res) => {
     res.status(400).send('something broke!')
